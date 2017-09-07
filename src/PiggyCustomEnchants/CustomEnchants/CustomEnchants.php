5255ebf43424b56e2eaf4a31f3324045c2efea4e
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-09-07 22:01:47 +0100

    Update CustomEnchants.php

--- src/PiggyCustomEnchants/CustomEnchants/CustomEnchants.php
+++ src/PiggyCustomEnchants/CustomEnchants/CustomEnchants.php
@@ -133,7 +133,7 @@ class CustomEnchants extends Enchantment
      * @param $name
      * @return $this|null|CustomEnchants
      */
-    public static function getEnchantmentBy($name)
+    public static function getEnchantmentByName($name)
     {
         if (defined(CustomEnchants::class . "::" . strtoupper($name))) {
             return self::getEnchantment(constant(CustomEnchants::class . "::" . strtoupper($name)));
